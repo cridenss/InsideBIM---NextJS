d78e43e9923510dd3eebce83829037b37dbce365
cridenss <bapt@reve@gmail.com>
2025-12-27 12:22:41 +0100

    post data and page layout setup

---
+++ app/components/pages/actualites/PostPageLayout/PostPageLayout.tsx
@@ -0,0 +1,9 @@
+import { posts, type PostKey } from "@/data/posts/posts";
+
+interface PostPageLayoutProps {
+    postKey: PostKey
+}
+
+export default function PostPageLAyout({postKey}: PostPageLayoutProps){
+    
+}
---
+++ data/posts/posts.ts
@@ -0,0 +1,23 @@
+export type PostKey = 
+| ''
+| ''
+| ''
+
+type PostData = {
+    id: string
+    title: string
+    description: string
+}
+
+export const posts = {
+    "test": {
+        id: "",
+        title: "",
+        dscription:""
+    },
+    "test1": {
+        id: "",
+        title: "",
+        dscription:""
+    },
+}
